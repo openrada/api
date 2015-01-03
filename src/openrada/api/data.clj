7f5b35116a78d8be4c6f5cbe31ecf016d21393d1
Anton Podviaznikov <podviaznikov@gmail.com>
2015-01-02 23:54:03 -0800

    fix seed method

--- src/openrada/api/data.clj
+++ src/openrada/api/data.clj
@@ -5,6 +5,7 @@
 
 
 (defn db-conf []
+  (println "host" (env :rethinkdb-host))
   {:host (env :rethinkdb-host)})
 
 (defn get-more-data-for-member [db-conn member]
@@ -17,7 +18,7 @@
 
 ;(db/update-member "0194b04d-4496-41b4-8458-ff34705e66c2" (collector/parse-member "http://gapp.rada.gov.ua/mps/info/page/15818"))
 
-(defn fetch-members-8-data []
+(defn seed-8-members []
   (println "init")
   (let [db-conn (db/make-connection (db-conf))
         members-8 (db/get-members-from-convocation db-conn 8)]
@@ -27,8 +28,9 @@
              (get-more-data-for-member db-conn member)) members-8)
       (let [members-8 (collector/parse-members-8)]
         (db/save-members db-conn members-8)
-        (map get-more-data-for-member (db/get-members-from-convocation db-conn 8))))))
+        (map (fn [member]
+             (get-more-data-for-member db-conn member)) (db/get-members-from-convocation db-conn 8))))))
 
 
 (defn init []
-  (fetch-members-8-data))
+  (seed-8-members))
