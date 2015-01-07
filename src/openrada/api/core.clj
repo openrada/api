12cf9e684076f8a0ba3dcba99d9579438d057419
Anton Podviaznikov <podviaznikov@gmail.com>
2015-01-07 00:52:02 -0800

    create two db connections

--- src/openrada/api/core.clj
+++ src/openrada/api/core.clj
@@ -11,15 +11,14 @@
   (let [{:keys [rethinkdb-host port]} config-options]
     (-> (component/system-map
           :config-options config-options
-          :db (new-database rethinkdb-host)
+          :db-read (new-database rethinkdb-host)
+          :db-write (new-database rethinkdb-host)
           :http (component/using
              (new-http-server port)
-             {:database  :db}
-          )
+             {:database :db-read})
           :data-collector (component/using
              (new-datacollector)
-             {:database  :db}
-          )))))
+             {:database :db-write})))))
 
 
 (def system (rada-system {:rethinkdb-host (env :rethinkdb-host) :port 3000}))
