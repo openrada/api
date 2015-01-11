1fe94ecabeacf8c88f8d7c6da3eaea0e77b95bbf
Anton Podviaznikov <podviaznikov@gmail.com>
2015-01-10 17:59:35 -0800

    start adding api docs

--- src/openrada/api/http.clj
+++ src/openrada/api/http.clj
@@ -25,12 +25,21 @@
 
     (defroutes app
       (ANY "/" [] (resource))
-      (GET "/v1/parliament/:convocation/members/:id" [convocation id]
-           (resource
-             :available-media-types ["application/json"]
-             :handle-ok (fn [ctx]
-                            (to-json (db/get-member-full database (read-string convocation) id)))))
 
+
+      ;;;;
+      ;; @api {get} /parliament/:convocation/members Get all members of the parliament from the specific convocation.
+      ;; @apiName GetMembers
+      ;; @apiGroup Members
+      ;; @apiParam {Number} convocation Parliament convocation. Current one is 8th.
+      ;; @apiExample {shell} Example usage:
+      ;;    curl -i https://api.openrada.com/v1/parliament/8/memebers
+      ;; @apiSuccess (200) {Object[]} members List of parliament members.
+      ;; @apiSuccess (200) {String} members.full_name Member's full name.
+      ;; @apiSuccess (200) {String} members.short_name Member's short name.
+      ;; @apiSuccess (200) {Date} members.dob Member's date of birth.
+      ;; @apiSuccess (200) {Date} members.member_since Date of becoiming a parliamnet member.
+      ;;;;
 
       (GET "/v1/parliament/:convocation/members" [convocation]
            (resource
@@ -38,6 +47,21 @@
              :handle-ok (fn [ctx]
                             (to-json (db/get-members-full database (read-string convocation))))))
 
+      ;;;;
+      ;; @api {get} /parliament/:convocation/members/:id Get full information for the specific parliament member.
+      ;; @apiName GetMember
+      ;; @apiGroup Members
+      ;; @apiParam {Number} convocation Parliament convocation. Current one is 8th.
+      ;; @apiParam {Number} id Parliament member id.
+      ;; @apiExample {shell} Example usage:
+      ;;    curl -i https://api.openrada.com/v1/parliament/8/memebers/005ded9a-18c4-4f34-806c-80a82e9a7a26
+      ;;;;
+
+      (GET "/v1/parliament/:convocation/members/:id" [convocation id]
+           (resource
+             :available-media-types ["application/json"]
+             :handle-ok (fn [ctx]
+                            (to-json (db/get-member-full database (read-string convocation) id)))))
 
       (GET "/v1/parliament/:convocation/factions/:id" [convocation id]
            (resource
