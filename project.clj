7ce150cf4fe9ae5b3a39aa16e4edfcde3ed690fd
Anton Podviaznikov <podviaznikov@gmail.com>
2015-01-15 01:10:20 -0800

    update url

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject openrada/api "0.1.0-SNAPSHOT"
   :description "Openrada API component"
-  :url "https://github.com/openrada/api"
+  :url "https://api.openrada.com"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :min-lein-version "2.0.0"
